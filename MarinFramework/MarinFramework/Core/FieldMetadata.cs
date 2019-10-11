1bdc2abc41a1891eaedb5ef891c790b326dbd1e4
Henka.Programmer@gmail.com <Henka.Programmer@gmail.com>
2019-10-11 22:22:57 +0100

    adds FieldMetadata class

---
+++ MarinFramework/MarinFramework/Core/FieldMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarinFramework.Core
+{
+    /// <summary>
+    /// Metadata for field classes.
+    /// </summary>
+    public abstract class FieldMetadata
+    {
+       
+    }
+}
